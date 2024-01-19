546507f31592d3609b87f503651fa27de3ceae15
sagar yenkure <yenkuresagar2104@gmail.com>
2024-01-19 17:21:39 +0530

    Firebase signup added

--- frontend/src/comp/Signup.jsx
+++ frontend/src/comp/Signup.jsx
@@ -1,62 +1,56 @@
 import React from "react";
 import Footer from "./Home/Footer";
 import logo from "../assets/logo.png";
-import { useState } from "react";
-import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
-import CheckBoxIcon from "@mui/icons-material/CheckBox";
-import {useNavigate} from "react-router-dom"
+import { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { app } from "../Auth/Firebase";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { useToast } from "@/components/ui/use-toast";
+import { Toaster } from "@/components/ui/toaster";
 
+const Login = () => {
+  const auth = getAuth(app);
+  const { toast } = useToast();
 
-const Signup = () => {
-  const host = "http://localhost:5000";
-
-  const navigate = useNavigate(); 
-  const [info, setinfo] = useState({
-    email:"",
-    password:""
-  })
+  const navigate = useNavigate();
   const [show, setshow] = useState(true);
+  const [errorbox, seterrorbox] = useState("");
   const [cheack, setcheack] = useState(true);
-  const [errorbox, seterrorbox] = useState("")
+  const [credentials, setcredentials] = useState({ email: "", password: "" });
 
-
-const onchange=(e)=>{
-  setinfo({
-    ...info,
-    [e.target.name]:e.target.value
-  })
-}
-const handlesumbit=async(e)=>{
-  e.preventDefault();
-  const responce=await fetch(`${host}/api/auth/login`,{
-    method:'post',
-    headers:{
-      'content-type':'application/json'
-    },
-    body:JSON.stringify({email:info.email,password:info.password})
-  });
-  const json = await responce.json()
-  if(json.sucess){
-    console.log(json)
-    navigate('/movies')
-  }
-  else{
-    seterrorbox(json.error)
-  }
-  setTimeout(() => {
-    seterrorbox("")
-    
-  }, 5000);
-
-
-}
+  const onchange = (e) => {
+    setcredentials({
+      ...credentials,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handlesubmit = async (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(
+      auth,
+      credentials.email,
+      credentials.password
+    )
+      .then((res) => {
+        console.log(res);
+        toast({
+          description: "Account created succesfuly",
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        toast({
+          variant: "destructive",
+          description: err.code,
+        });
+      });
+  };
 
   const handleshow = () => {
     if (show) {
       setshow(false);
     } else if (!show) {
       setshow(true);
-      
     }
   };
   const handlecheck = () => {
@@ -68,27 +62,27 @@ const handlesumbit=async(e)=>{
   };
   return (
     <>
-      <div className=" relative home__screen bg-black lg:bg-home bg-fit bg-cover  w-full h-fit lg:h-screen ">
-        <div className="bg-[rgba(0,0,0,0.7)]  pl-2 lg:pl-5">
+      <div className=" relative home__screen bg-black lg:bg-home bg-fit bg-cover  w-full h-fit lg:h-screen">
+        <div className="bg-[rgba(0,0,0,0.7)]  pl-2 lg:pl-5 py-5">
           {/* netflix logo  */}
           <img className=" w-[6rem] lg:w-[12rem]  " src={logo} alt="logo" />
         </div>
 
-        <div className="bg w-full h-full  flex flex-col items-center  bg-[rgba(0,0,0,0.7)] border-b border-gray-600 pb-[20%]">
+        <div className="bg w-full h-full  flex flex-col items-center  bg-[rgba(0,0,0,0.7)] border-b border-gray-600 pb-[20%] ">
           <div className=" boxxx w-full lg:w-[30%] h-[70%] lg:h-screen bg-black lg:bg-[rgba(0,0,0,0.7)]">
             <div className=" main box__ p-5 lg:p-[15%]">
-            <h1 className="text-white font-bold text-xl">{errorbox}</h1>
-              <h1 className=" text-white font-bold text-3xl">Sign In</h1>
+              <h1 className="text-white font-bold text-xl">{errorbox}</h1>
+              <h1 className=" text-white font-bold text-3xl">
+                Create an Account
+              </h1>
               {/* text filed for Email */}
               <div className=" pt-[5%] w-full  py-4 flex flex-col lg:flex-row h-full  ">
                 <input
                   className=" p-4 bg-gray rounded-md text bg-[#333333] text-white lg:w-full h-fit"
                   type="email"
                   name="email"
-                  id="email"
-                  value={info.email}
+                  value={credentials.email}
                   onChange={onchange}
-
                   placeholder="Email or Phone Number"
                   required
                 />
@@ -99,9 +93,8 @@ const handlesumbit=async(e)=>{
                   className="  p-4 bg-gray text-white rounded-md text bg-[#333333]  lg:w-full h-fit"
                   type={show ? "Password" : "text"}
                   name="password"
-                  id="pass"
                   placeholder="Password"
-                  value={info.password}
+                  value={credentials.password}
                   onChange={onchange}
                   required
                 />
@@ -116,25 +109,24 @@ const handlesumbit=async(e)=>{
               </div>
 
               {/* sign in button  */}
-              <div onClick={handlesumbit} className=" bg-[#e50914] mt-[10%] text-center w-full rounded-md py-4 h-full  ">
+              <div
+                onClick={handlesubmit}
+                className=" bg-[#e50914] mt-[10%] text-center w-full rounded-md py-4 h-full  "
+              >
                 <button className="text-center text-white font-bold  ">
-                  Sign In
+                  Sign Up
                 </button>
               </div>
-              <div className="box text-gray-400 text-sm flex justify-between pt-3">
-                <div className="cheaked__box">
-                  <button onClick={handlecheck}>
-                    {cheack ? <CheckBoxOutlineBlankIcon /> : <CheckBoxIcon />}
-                  </button>
-                  Remember Me
-                </div>
-                <p className="needhelp__"></p>
-                need Help ?
-              </div>
+
               {/* new to netflix section  */}
               <div className="flex p-1 space-x-1">
-                <p className="py-3 text-gray-400">New to Netflix?</p>
-                <button onClick={()=>navigate("/login")} className="font-semibold text-white">Sign Up now</button>
+                <p className="py-3 text-gray-400">already has account?</p>
+                <button
+                  onClick={() => navigate("/Login")}
+                  className="font-semibold text-white"
+                >
+                  login now
+                </button>
               </div>
               <div className="text-sm">
                 <p className="text-gray-400">
@@ -146,10 +138,11 @@ const handlesumbit=async(e)=>{
             </div>
           </div>
         </div>
+        <Toaster />
       </div>
       <Footer />
     </>
   );
 };
 
-export default Signup;
+export default Login;
